1e26e5cd4c4ea4ee51ff73e97b025385d9b122fe
Guilherme Thomas <gui.thomas@outlook.com>
2022-02-03 00:52:35 -0300

    Update keepHydrated.java
    Adicionar catch?

--- keepHydrated/keepHydrated.java
+++ keepHydrated/keepHydrated.java
@@ -1,4 +1,7 @@
 public class KeepHydrated  {
+  
+      if ( time < 0 ) 
+      throw new IllegalArgumentException("non-positive number");
 
   public int Liters(double time)  {
     return (int)(time * .5);
